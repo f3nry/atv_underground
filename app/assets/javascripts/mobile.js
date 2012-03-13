daabcf0c81f8db7ccb7a39b43a789b1561160521
JAARS Aviation Training <JAARS_AVIATION@airventure-1s-imac.local>
2012-03-13 17:44:56 -0400

    another days work

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -10,6 +10,16 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
-// 
+//= require sencha-touch-all
+//= require_self
+
+Ext.application({
+	name: 'Avt',
+
+	controllers: [ 'Main' ],
+	views: [ 'Main' ],
+
+	launch: function() {
+    Ext.create('Avt.view.Main');
+	}
+});
